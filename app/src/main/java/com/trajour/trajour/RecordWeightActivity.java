db8bd2eb01ba0388fb7ad5f6d2b36da2ab4be327
kenzo <kenzonakase@gmail.com>
2018-04-10 07:27:33 +0900

    Ver.3

--- app/src/main/java/com/trajour/trajour/RecordWeightActivity.java
+++ app/src/main/java/com/trajour/trajour/RecordWeightActivity.java
@@ -1,11 +1,45 @@
 package com.trajour.trajour;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RecordWeightActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecordWeightActivity extends AppCompatActivity implements View.OnClickListener, DatabaseReference.CompletionListener {
+
+    private Toolbar mToolbar;
+    private ProgressDialog mProgress;
+    private EditText mEditDate1;
+    private EditText mEditHeight1;
+    private EditText mEditWeight1;
+    private EditText mEditBodyFatPercentage1;
+    private Button mSaveButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +48,75 @@ public class RecordWeightActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // UIの準備
+        setTitle("体重・体脂肪率を記録する");
+
+        mEditDate1 = (EditText) findViewById(R.id.editDate1);
+        mEditHeight1 = (EditText) findViewById(R.id.editHeight1);
+        mEditWeight1 = (EditText) findViewById(R.id.editWeight1);
+        mEditBodyFatPercentage1 = (EditText) findViewById(R.id.editBodyFatPercentage1);
+        mEditDate1 = (EditText) findViewById(R.id.editDate1);
+
+        mSaveButton = (Button) findViewById(R.id.saveButton);
+        mSaveButton.setOnClickListener(this);
+
+        mProgress = new ProgressDialog(this);
+        mProgress.setMessage("投稿中...");
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == mSaveButton) {
+            // キーボードが出てたら閉じる
+            InputMethodManager im = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+            DatabaseReference dataBaseReference = FirebaseDatabase.getInstance().getReference();
+            DatabaseReference genreRef = dataBaseReference.child(Const.ContentsPATH).child(String.valueOf(mGenre));
+
+            Map<String, String> data = new HashMap<String, String>();
+
+            // UID
+            data.put("uid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+            // タイトルと本文を取得する
+            String title = mTitleText.getText().toString();
+            String body = mBodyText.getText().toString();
+
+            if (title.length() == 0) {
+                // 質問が入力されていない時はエラーを表示するだけ
+                Snackbar.make(v, "タイトルを入力して下さい", Snackbar.LENGTH_LONG).show();
+                return;
+            }
+
+            if (body.length() == 0) {
+                // 質問が入力されていない時はエラーを表示するだけ
+                Snackbar.make(v, "質問を入力して下さい", Snackbar.LENGTH_LONG).show();
+                return;
+            }
+
+            // Preferenceから名前を取る
+            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+            String name = sp.getString(Const.NameKEY, "");
+
+            data.put("title", title);
+            data.put("body", body);
+            data.put("name", name);
+
+            genreRef.push().setValue(data, this);
+            mProgress.show();
+        }
+    }
+
+    @Override
+    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+        mProgress.dismiss();
+
+        if (databaseError == null) {
+            finish();
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), "投稿に失敗しました", Snackbar.LENGTH_LONG).show();
+        }
+    }
 }
